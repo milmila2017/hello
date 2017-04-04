8743faaa4124810b1763b979fa8e8d0adf23d939
Mahadev <Mahadev@192.168.1.5>
2017-04-04 20:25:35 +0800

    add system out println

---
+++ twotwo/src/twotwo/test.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package twotwo;
+
+/**
+ * @author Mahadev
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//test
+		System.out.print("hello");
+	}
+
+}
